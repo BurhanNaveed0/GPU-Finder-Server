16b9326a6cd3efb03c4606c5b3b2e44233c77384
BurhanNaveed0 <burhannaveed0@gmail.com>
2022-06-03 11:56:07 -0400

    Data fetcher method almost completed

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import requests
 import json
+import firebase_admin
+import schedule
 
 params_3060 = {
-  'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
-  'type': 'search',
-  'amazon_domain': 'amazon.com',
-  'search_term': 'RTX 3060',
-  'sort_by': ''
+    'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
+    'type': 'search',
+    'amazon_domain': 'amazon.com',
+    'search_term': 'RTX 3060',
+    'sort_by': 'price_low_to_high'
 }
 
 params_3070 = {
-  'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
-  'type': 'search',
-  'amazon_domain': 'amazon.com',
-  'search_term': 'RTX 3070',
-  'sort_by': 'price_high_to_low'
+    'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
+    'type': 'search',
+    'amazon_domain': 'amazon.com',
+    'search_term': 'RTX 3070',
+    'sort_by': 'price_low_to_high'
 }
 
 params_3080 = {
-  'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
-  'type': 'search',
-  'amazon_domain': 'amazon.com',
-  'search_term': 'RTX 3080',
-  'sort_by': 'price_low_to_high'
+    'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
+    'type': 'search',
+    'amazon_domain': 'amazon.com',
+    'search_term': 'RTX 3080',
+    'sort_by': 'price_low_to_high'
 }
 
 def fetch_data_product(params):
@@ -33,27 +35,49 @@ def fetch_data_product(params):
     # EXTRACT SEARCH RESULT DATA
     search_results = api_response['search_results']
 
-    # REFINE SEARCH RESULT DATA
+    # EXCLUDE UNWANTED SEARCH RESULT DATA
     search_results_refined = []
 
     for x in range(0, 10):
-      search_results_refined.append(search_results[x])
+        search_results_refined.append(search_results[x])
 
     # FORMAT DATA
+    filtered_search_results = []
+
     for search in search_results_refined:
-      name = search["title"]
-      link = search["link"]
-      
-    return search_results_refined
+        name = search["title"]
+
+        try:
+            price = search["price"]["value"]
+        except:
+            price = "No Price Listed"
+
+        image = search["image"]
+        link = search["link"]
+
+        formatted_json = {
+            "name" : name,
+            "price" : price,
+            "image" : image,
+            "link" : link
+        }
+
+        filtered_search_results.append(formatted_json)
+
+    return filtered_search_results
 
 def upload_data():
-    print('hello')
+    data_3060 = fetch_data_product(params_3060)
+    data_3070 = fetch_data_product(params_3070)
+    data_3080 = fetch_data_product(params_3080)
+
 
 def main():
     api_response = fetch_data_product(params_3070)
     print(json.dumps(api_response))
 
 main()
+#schedule.every(10).minutes.do(upload_data())
 
 
 
