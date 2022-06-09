566dbb1911dd6279ce701fa4a448871f71a02ea1
BurhanNaveed0 <burhannaveed0@gmail.com>
2022-06-09 11:59:32 -0400

    Firebase Data Store Working

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import json
-import firebase_admin
+import pyrebase
 import schedule
 
+# SEARCH PARAMETERS
 params_3060 = {
     'api_key': '78C3B9733F7840B7B25DDE8CC464A3D7',
     'type': 'search',
@@ -26,6 +27,21 @@ params_3080 = {
     'search_term': 'RTX 3080',
     'sort_by': 'price_low_to_high'
 }
+
+# DATABASE CONFIGURATION
+config = {
+  "apiKey": "AIzaSyADEPTyHHzEXAgSdW0kMC7EyHTlV1rS_b8",
+  "authDomain": "gpu-finder-database.firebaseapp.com",
+  "databaseURL": "https://gpu-finder-database-default-rtdb.firebaseio.com",
+  "projectId": "gpu-finder-database",
+  "storageBucket": "gpu-finder-database.appspot.com",
+  "messagingSenderId": "85929553382",
+  "appId": "1:85929553382:web:1236442a8e7798bd997a27",
+  "measurementId": "G-MN1138YJLV"
+}
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database();
 
 def fetch_data_product(params):
     # MAKE API CALL
@@ -67,17 +83,17 @@ def fetch_data_product(params):
     return filtered_search_results
 
 def upload_data():
+    # FETCH PRODUCT DATA
     data_3060 = fetch_data_product(params_3060)
     data_3070 = fetch_data_product(params_3070)
     data_3080 = fetch_data_product(params_3080)
 
+    # PUSH SEARCH DATA
+    database.child("3060").set(data_3060)
+    database.child("3070").set(data_3070)
+    database.child("3080").set(data_3080)
 
-def main():
-    api_response = fetch_data_product(params_3070)
-    print(json.dumps(api_response))
-
-main()
-#schedule.every(10).minutes.do(upload_data())
+schedule.every(10).minutes.do(upload_data())
 
 
 
